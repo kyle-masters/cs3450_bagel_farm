39c70472b3c73f26b8d93d7da0d80415a0a35579
Dallin-Larsen <56076097+Dallin-Larsen@users.noreply.github.com>
2020-10-07 16:46:06 -0600

    Edits to the frontend login page

--- dev/frontEnd/bagelfarm/src/App.js
+++ dev/frontEnd/bagelfarm/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Switch , Route } from 'react-router-dom'
-import logo from './logo.svg';
+import { Switch , Route } from 'react-router-dom';
 import './App.css';
-import Layout from './hoc/Layout/Layout'
+import Layout from './hoc/Layout/Layout';
+import LoginPage from './pages/loginpage/logainpage';
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Switch>
-          
+          <Route path='/login' component={LoginPage} />
         </Switch>
       </Layout>
       
